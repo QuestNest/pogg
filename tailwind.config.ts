b05cd972be216ca71dc6cabe1d9348f0bc869f2c
MikaelVallenet <mikael.vallenetpro@gmail.com>
2024-11-24 17:26:40 +0100

    feat: add background & color themes

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,9 +8,18 @@ export default {
   ],
   theme: {
     extend: {
+      backgroundImage: {
+        "home-bg": "url('/background-syndra.jpg')",
+      },
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        "background": "#64113F",
+        "light-background": "#75204F",
+        "blush": "#DE4D86",
+        "salmon-pink": "F29CA3",
+        "tea-rose": "#F7CACD",
+        "non-photo-blue": "#84E6F8",
+        "grey": "#A9A9AC",
+        "white": "#FFFFFF",
       },
     },
   },
