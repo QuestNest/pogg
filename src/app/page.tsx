e04bc4fe337ded1b7d601023ee4a1ecaa0cb2963
MikaelVallenet <mikael.vallenetpro@gmail.com>
2024-12-01 17:03:41 +0100

    feat: add PO.GG title

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,11 @@
+import Title from "@/components/Title";
 
 export default function Home() {
   return (
     <div className="bg-home-bg bg-cover bg-left bg-no-repeat h-screen">
-      <p className="text-2xl font-bold text-center">Welcome to POGG</p>
+      <div className="absolute top-[20%] left-1/2 transform -translate-x-1/2">
+        <Title />
+      </div>
     </div>
   );
 }
