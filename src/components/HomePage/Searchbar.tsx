445aebfc2ae899c941221dec2cdc94cc28708088
MikaelVallenet <mikael.vallenetpro@gmail.com>
2024-12-01 20:45:45 +0100

    feat: add the search bar

--- src/components/HomePage/Searchbar.tsx
+++ src/components/HomePage/Searchbar.tsx
@@ -1,13 +1,18 @@
+import { inter } from "@/fonts/fonts";
+
 const Searchbar = () => {
     return (
-        <div className="relative w-full mt-8">
+        <div className={`relative w-full mt-8 ${inter.className} font-semibold`}>
             <input
                 type="text"
                 placeholder="Search Yourself or a Champion"
-                className="w-full py-4 pr-24 pl-4 text-xl text-gray-700 bg-white border border-purple-500 rounded-lg shadow-md focus:outline-none focus:ring-2 focus:ring-purple-500 focus:border-purple-500"
+                className="w-full py-4 pr-24 pl-4 text-xl text-stone-600 bg-white rounded-lg shadow-md focus:outline-none"
             />
-            <button className="absolute top-1/2 right-4 transform -translate-y-1/2 text-purple-500">
-                Search
+            <button className="absolute top-1/2 right-4 transform -translate-y-1/2 text-dark-gray-700">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" className="size-6">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z" />
+                </svg>
+
             </button>
         </div>
     );
