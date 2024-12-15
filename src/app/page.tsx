dc4fcfae10d688af92492bbe064645e99826f8dc
MikaelVallenet <mikael.vallenetpro@gmail.com>
2024-12-15 17:54:09 +0100

    feat: add navbar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,16 @@
 import Searchbar from "@/components/HomePage/Searchbar";
 import Title from "@/components/HomePage/Title";
+import SideNav from "@/components/SideNav";
 
 export default function Home() {
   return (
-    <div className="bg-home-bg bg-cover bg-center bg-no-repeat h-dvh flex justify-center">
-      <div className="absolute top-[20%] w-[70%] max-w-[600px] text-center">
-        <Title />
-        <Searchbar />
+    <div className="bg-home-bg bg-cover bg-center bg-no-repeat h-dvh">
+      <SideNav />
+      <div className="flex justify-center">
+        <div className="absolute top-[20%] w-[70%] max-w-[600px] text-center">
+          <Title />
+          <Searchbar />
+        </div>
       </div>
     </div>
   );
---
+++ src/components/SideNav.tsx
@@ -0,0 +1,40 @@
+import { inter } from "@/fonts/fonts";
+
+const SideNav = () => {
+    return (
+        <div
+            className="relative flex h-full w-full max-w-[15rem] flex-col bg-light-background p-4 text-gray-700 shadow-xl shadow-blue-gray-900/5">
+            <div className="p-4 mb-2">
+                <h2 className={`sm:text-5xl text-6xl font-extrabold text-white drop-shadow-lg ${inter.className}`}>
+                    <span className="text-blush">P</span>O.GG
+                </h2>
+            </div>
+            <hr className="my-4 h-0.5 border-t-0 bg-neutral-100 dark:bg-white/10" />
+            <nav className="flex min-w-[240px] flex-col gap-1 p-2 font-sans text-base text-white ${inter.className} font-semibold">
+                <div role="button"
+                    className="flex items-center w-full p-3 leading-tight transition-all rounded-lg outline-none text-start hover:bg-blue-gray-50 hover:bg-opacity-80 hover:text-blue-gray-900 focus:bg-blue-gray-50 focus:bg-opacity-80 focus:text-blue-gray-900 active:bg-blue-gray-50 active:bg-opacity-80 active:text-blue-gray-900">
+                    <div className="grid mr-4 place-items-center">
+                        <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#FFFFFF"><path d="M480-480q-66 0-113-47t-47-113q0-66 47-113t113-47q66 0 113 47t47 113q0 66-47 113t-113 47ZM160-160v-112q0-34 17.5-62.5T224-378q62-31 126-46.5T480-440q66 0 130 15.5T736-378q29 15 46.5 43.5T800-272v112H160Zm80-80h480v-32q0-11-5.5-20T700-306q-54-27-109-40.5T480-360q-56 0-111 13.5T260-306q-9 5-14.5 14t-5.5 20v32Zm240-320q33 0 56.5-23.5T560-640q0-33-23.5-56.5T480-720q-33 0-56.5 23.5T400-640q0 33 23.5 56.5T480-560Zm0-80Zm0 400Z" /></svg>
+                    </div>
+                    Champions
+                </div>
+                <div role="button"
+                    className="flex items-center w-full p-3 leading-tight transition-all rounded-lg outline-none text-start hover:bg-blue-gray-50 hover:bg-opacity-80 hover:text-blue-gray-900 focus:bg-blue-gray-50 focus:bg-opacity-80 focus:text-blue-gray-900 active:bg-blue-gray-50 active:bg-opacity-80 active:text-blue-gray-900">
+                    <div className="grid mr-4 place-items-center">
+                        <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#FFFFFF"><path d="M280-280h80v-200h-80v200Zm320 0h80v-400h-80v400Zm-160 0h80v-120h-80v120Zm0-200h80v-80h-80v80ZM200-120q-33 0-56.5-23.5T120-200v-560q0-33 23.5-56.5T200-840h560q33 0 56.5 23.5T840-760v560q0 33-23.5 56.5T760-120H200Zm0-80h560v-560H200v560Zm0-560v560-560Z" /></svg>
+                    </div>
+                    Leaderboards
+                </div>
+                <div role="button"
+                    className="flex items-center w-full p-3 leading-tight transition-all rounded-lg outline-none text-start hover:bg-blue-gray-50 hover:bg-opacity-80 hover:text-blue-gray-900 focus:bg-blue-gray-50 focus:bg-opacity-80 focus:text-blue-gray-900 active:bg-blue-gray-50 active:bg-opacity-80 active:text-blue-gray-900">
+                    <div className="grid mr-4 place-items-center">
+                        <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#FFFFFF"><path d="M762-96 645-212l-88 88-28-28q-23-23-23-57t23-57l169-169q23-23 57-23t57 23l28 28-88 88 116 117q12 12 12 28t-12 28l-50 50q-12 12-28 12t-28-12Zm118-628L426-270l5 4q23 23 23 57t-23 57l-28 28-88-88L198-96q-12 12-28 12t-28-12l-50-50q-12-12-12-28t12-28l116-117-88-88 28-28q23-23 57-23t57 23l4 5 454-454h160v160ZM334-583l24-23 23-24-23 24-24 23Zm-56 57L80-724v-160h160l198 198-57 56-174-174h-47v47l174 174-56 57Zm92 199 430-430v-47h-47L323-374l47 47Zm0 0-24-23-23-24 23 24 24 23Z" /></svg>
+                    </div>
+                    Items
+                </div>
+            </nav>
+        </div>
+    );
+};
+
+export default SideNav;
