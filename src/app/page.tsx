d9d3e5c0489a3a46651d5b2fb998328a4a5493ea
MikaelVallenet <mikael.vallenetpro@gmail.com>
2024-12-15 20:10:35 +0100

    feat: add footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,22 @@
+import Footer from "@/components/Footer";
 import Searchbar from "@/components/HomePage/Searchbar";
 import Title from "@/components/HomePage/Title";
 import SideNav from "@/components/SideNav";
 
 export default function Home() {
   return (
-    <div className="bg-home-bg bg-cover bg-center bg-no-repeat h-dvh relative">
-      <div className="grid grid-cols-[auto,1fr] h-full">
+    <div className="bg-home-bg bg-cover bg-center bg-no-repeat h-dvh grid grid-cols-[auto,1fr] grid-rows-[1fr,auto]">
+      <div className="row-span-2 h-full">
         <SideNav />
-        <div className="relative flex items-start justify-center">
-          <div className="absolute top-[20%] w-[70%] max-w-[600px] text-center">
-            <Title />
-            <Searchbar />
-          </div>
+      </div>
+      <div className="relative flex items-start justify-center row-span-1">
+        <div className="mt-36 w-[70%] max-w-[600px] text-center">
+          <Title />
+          <Searchbar />
         </div>
       </div>
+      <Footer />
     </div>
   );
 }
+
